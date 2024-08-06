b8349b21e280426034156ac52d983a0b2172f5de
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-08-06 23:06:00 +0600

    Product and Review model updated

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User;
 
 class Review extends Model
 {
-    protected $fillable = ['comment', 'rating'];
+    protected $fillable = ['comment', 'rating', 'user_id', 'product_id'];
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
